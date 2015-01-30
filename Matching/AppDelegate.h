a88e498fc2eb26f31f203fea499acdb370d1a931
John Schindler <john.schindler@utexas.edu>
2015-01-29 20:19:24 -0600

    Initial Commit

---
+++ Matching/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matching
+//
+//  Created by John  Schindler on 1/29/15.
+//  Copyright (c) 2015 John  Schindler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matching/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matching
+//
+//  Created by John  Schindler on 1/29/15.
+//  Copyright (c) 2015 John  Schindler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
